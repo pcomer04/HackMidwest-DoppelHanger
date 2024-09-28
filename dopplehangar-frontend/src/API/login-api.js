c208edcdb072f0159b0a0e7b644cfda996b529f6
Preston Ward <pward6@huskers.unl.edu>
2024-09-28 14:48:04 -0500

    updated api endpoints

--- dopplehangar-frontend/src/API/login-api.js
+++ dopplehangar-frontend/src/API/login-api.js
@@ -1,5 +1,5 @@
 export const login = async (username, password) => {
-    const response = await fetch("http://127.0.0.1:8000/login", {
+    const response = await fetch("http://127.0.0.1:8000/login/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
