11bd92ef663c69803d2ba8fee12a883226ca38e6
Charlie McIver <cmciver2@huskers.unl.edu>
2024-09-28 20:55:19 -0500

    added mathod to call imagecompare

--- backend/matching/image_compare.py
+++ backend/matching/image_compare.py
@@ -12,31 +12,31 @@ print(device)
 
 dataset_folder = './dataset/'
 
-images = []
-for root, dirs, files in os.walk(dataset_folder):
-    for file in files:
-        if file.endswith(('jpg','jpeg')):
-            images.append(  root  + '/'+ file)
 
+def compare_image(input_image_path):
+    # Load and preprocess the input image
+    input_image = preprocess(Image.open(input_image_path)).unsqueeze(0).to(device)
+    input_image_features = model.encode_image(input_image)
 
-#Embedding of the input image
-input_image = preprocess(Image.open("images/khakis.jpg")).unsqueeze(0).to(device)
-input_image_features = model.encode_image(input_image)
+    images = []
+    for root, dirs, files in os.walk(dataset_folder):
+        for file in files:
+            if file.endswith(('jpg', 'jpeg')):
+                images.append(root + '/' + file)
 
-result = {}
-for img in images:
-    with torch.no_grad():
-        image_preprocess = preprocess(Image.open(img)).unsqueeze(0).to(device)
-        image_features = model.encode_image( image_preprocess)
-        cos = torch.nn.CosineSimilarity(dim=0)
-        sim = cos(image_features[0],input_image_features[0]).item()
-        sim = (sim+1)/2
-        result[img]=sim
+    result = {}
+    for img in images:
+        with torch.no_grad():
+            image_preprocess = preprocess(Image.open(img)).unsqueeze(0).to(device)
+            image_features = model.encode_image(image_preprocess)
+            cos = torch.nn.CosineSimilarity(dim=0)
+            sim = cos(image_features[0], input_image_features[0]).item()
+            sim = (sim + 1) / 2
+            result[img] = sim
 
+    sorted_value = sorted(result.items(), key=lambda x: x[1], reverse=True)
+    sorted_res = dict(sorted_value)
 
-sorted_value = sorted(result.items(), key=lambda x:x[1], reverse=True)
-sorted_res = dict(sorted_value)
+    top_3 = dict(itertools.islice(sorted_res.items(), 3))
 
-top_3 = dict(itertools.islice(sorted_res.items(), 3))
-
-print(top_3)
+    return top_3
